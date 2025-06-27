2b78d0cd601ee2efa8394f7dbf82100f28f312e7
Marcel Grace <your.email@example.com>
2025-06-27 15:32:40 +0600

    error fixing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={museoModerno.variable}>
+    <html lang="en" className={museoModerno.variable} suppressHydrationWarning>
       <body className={`${museoModerno.className} antialiased`}>
         <div className="flex flex-col min-h-screen">
           <Navigation />
